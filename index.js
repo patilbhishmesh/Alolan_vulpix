e01966445dd495d3a8ffcfab610c763959e4d89a
patilbhishmesh <patilbhishmesh@gmail.com>
2025-11-12 21:34:40 +0530

    Implement keep-alive web server using Express
    Added a keep-alive web server to respond to requests.

--- index.js
+++ index.js
@@ -1015,3 +1015,9 @@ try {
     await message.channel.send({ embeds: [embed] });
   }
 });
+// --- Keep-alive web server for Render ---
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is alive!"));
+app.listen(3000, () => console.log("Ping server active"));
