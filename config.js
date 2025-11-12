63b7aa35f461cb4c57d36e6682b9362d843c9a07
patilbhishmesh <patilbhishmesh@gmail.com>
2025-11-12 19:23:01 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  botToken: "MTQyNzM2Njc0MTE4NDQ4MzM5OA.GK-hOx.NPYsxxH8B1-PZV735nNUmuaRV3HE4haEntJurs",
+  prefix: "!",
+  owners: ["ownerid"],
+  captchaHook: "https://discord.com/api/webhooks/1425142744132747264/sET4rq44f1pxv34ig2JH1IbPrc1YMxhnHxTJ-A9yeTtlYBfMQQwF-r8Q4Xa_zw_i4WVg",
+  logHook: "https://discord.com/api/webhooks/1425142727787548683/hmJzLwQjvXfVWAixeEU4PTOBBVTVLnhQsbPqAe0uhq-bKrjPrfJyQf6GfQpv-R4BjwP4",
+  webHook: "https://discord.com/api/webhooks/1425143236061696174/lnz2XEePFkSvLv0MP2nwV534ZmwaRk9ocARCiVq6zdFeRoofTQgjYPXB_pGwW42kU1PM",
+  captchaApiKey: "api-key",
+  captchaApiHostname: "http://217.154.201.164:9701"
+};
