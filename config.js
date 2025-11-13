ecd696e606810bf40a415377af02bb1cd2a661e2
patilbhishmesh <patilbhishmesh@gmail.com>
2025-11-13 07:55:23 +0530

    Modify config.js with new webhooks and owners
    Updated webhook URLs and added new owners.

--- config.js
+++ config.js
@@ -2,18 +2,11 @@ module.exports = {
 
   botToken: process.env.TOKEN,
 
-  prefix: "!",
-
-  owners: ["1035446256736161792"],
-
-  captchaHook: "https://discord.com/api/webhooks/1437843097458446426/doJ3WWZO4mc8iDMLdQLYwdV7S5FB5MSvuIDMnpZXLnQVN3hZSFNzIog4ZfmfSHrbyTtF",
-
-  logHook: "https://discord.com/api/webhooks/1437843097458446426/doJ3WWZO4mc8iDMLdQLYwdV7S5FB5MSvuIDMnpZXLnQVN3hZSFNzIog4ZfmfSHrbyTtF",
-
-  webHook: "https://discord.com/api/webhooks/1437843097458446426/doJ3WWZO4mc8iDMLdQLYwdV7S5FB5MSvuIDMnpZXLnQVN3hZSFNzIog4ZfmfSHrbyTtF",
-
+   prefix: "!",
+  owners: ["1035446256736161792","1079670579885506640","1010979614631407667","797883209895116840"],
+  captchaHook: "https://discord.com/api/webhooks/1428303809498714173/EAw-dJk5MfxIvBhevPFC9ER2MsHibP6oTWpWzEeJorzfJ78k3LKMgFamaN1CSJ6CKcKZ",
+  logHook: "https://discord.com/api/webhooks/1428415013487378554/FH5-dKGUtVyJ2JcMnK0o9ZMNA52fv5ai3SFljyuqGOjLo2TWCkXwtfxKWKw3EvqaSN6j",
+  webHook: "https://discord.com/api/webhooks/1428414553061855334/-p-HzSLh-LDs1psByHSuSkRjcp_2kAGMWOv_UTPWa6-Ztb_xK846J4XAEtSQFnbyRp4G",
   captchaApiKey: "SHUURECAPV2-51318147-4A24-4B1B-9055-6F63F6D06873",
-
   captchaApiHostname: "http://217.154.201.164:9701"
-
 };
