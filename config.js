a815d1053a421fe76419be9696ae6a9d535b2591
patilbhishmesh <patilbhishmesh@gmail.com>
2025-11-19 00:27:15 +0530

    Update captcha API key in config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
   captchaHook: "https://discord.com/api/webhooks/1428303809498714173/EAw-dJk5MfxIvBhevPFC9ER2MsHibP6oTWpWzEeJorzfJ78k3LKMgFamaN1CSJ6CKcKZ",
   logHook: "https://discord.com/api/webhooks/1428415013487378554/FH5-dKGUtVyJ2JcMnK0o9ZMNA52fv5ai3SFljyuqGOjLo2TWCkXwtfxKWKw3EvqaSN6j",
   webHook: "https://discord.com/api/webhooks/1428414553061855334/-p-HzSLh-LDs1psByHSuSkRjcp_2kAGMWOv_UTPWa6-Ztb_xK846J4XAEtSQFnbyRp4G",
-  captchaApiKey: "SHUURECAPV2-51318147-4A24-4B1B-9055-6F63F6D06873",
+  captchaApiKey: "SHUURECAPV2-48B2C6D0-026E-484B-8937-242DD97CF45C",
   captchaApiHostname: "http://217.154.201.164:9701"
 };
